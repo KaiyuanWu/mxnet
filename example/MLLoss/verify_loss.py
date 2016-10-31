cb289dac8e0620c815a5be64407da8fd07a69c46
Kaiyuan Wu <kaiwu@buaa.edu.cn>
2016-10-31 20:35:38 +0800

    Update verify_loss.py

--- example/MLLoss/verify_loss.py
+++ example/MLLoss/verify_loss.py
@@ -1,183 +1,121 @@
 # -*- coding: utf-8 -*-
-#简化veryfy_loss的逻辑
-#只考虑k-nearest neightbor的genuine pair
+# 简化veryfy_loss的逻辑
+# 只考虑k-nearest neightbor的genuine pair
 #     误差最大的k个imposter triplet
 import numpy as np
-#import pairwise_distance_gpu as pdg
-from sklearn.metrics import pairwise_distances as pdg
+import pairwise_distance_gpu as pdg_gpu
+from sklearn.metrics import pairwise_distances as pdg_cpu
 from scipy.sparse import coo_matrix
 from collections import Counter
+import  verify_loss_c_py as vlc
 
 class VerifyLoss:
-    def __init__(self, margin = 1, mu = 0.5):
-        self.margin = margin
-        self.mu = mu
-        
-    def get_mu(self):
-        return self.mu
-    
-    def get_margin(self):
-        return self.margin
-    
-    def approx_gradient(self, data, genuine_pair, imposter_triplet):
+    def __init__(self, nclasses, nfeats, margin=1):
+        self.margin_ = margin
+        self.nclasses_ = nclasses
+        self.nfeats_ = nfeats
+        # self.centers_ = np.zeros((nclasses, nfeats), dtype = 'float32')
+        self.centers_ = np.random.randn(nclasses, nfeats).astype('float32')
+
+    def approx_gradient(self, data, triplet):
         grad = np.copy(data)
         epsilon = 1.
         for i in range(grad.shape[0]):
             for j in range(grad.shape[1]):
                 data0 = np.copy(data)
-                data0[i,j] = data[i,j] - 0.5*epsilon
-                f0 = self.eval(data0, genuine_pair, 0, imposter_triplet,0)
-                data0[i,j] = data[i,j] + 0.5*epsilon
-                f1 = self.eval(data0, genuine_pair, 0, imposter_triplet, 0)
-                grad[i,j] = (f1 - f0)/epsilon
-        return grad        
-    
-    def forward_backward(self, data, label, genuine_label,  k=3, calculate_loss = True):
-        self._select_knn(data, label, genuine_label, k)
-        
-        genuine_pair, genuine_pair_dist = self._select_genuine_pair(label, genuine_label)
-        imposter_triplet, imposter_triplet_dist = self._select_imposter_triplet(data, label, genuine_label)
-        
-        nsamples = data.shape[0]
-        
-        AG = coo_matrix((genuine_pair[...,0]*0-1, (genuine_pair[...,0], genuine_pair[...,1])), shape=(nsamples, nsamples))
-        AG_row_sum = AG.sum(axis = 0)
-        AG_col_sum = AG.sum(axis = 1)
-	T = coo_matrix((-0.5*(AG_row_sum.getA1() + AG_col_sum.getA1()), (range(nsamples), range(nsamples))), shape=(nsamples, nsamples))
-	AG = AG + T 
-        AG = AG + AG.T
-        
-        if imposter_triplet.shape[0]>0:
-            BIs = Counter(imposter_triplet[...,0]*nsamples + imposter_triplet[...,1])
-            k = np.array(BIs.keys(), dtype='int')
-            v = np.array(BIs.values())*(-1)
-            i = k%nsamples
-            j = k/nsamples
-            BI = coo_matrix((v, (i,j)), shape=(nsamples, nsamples))
-            BI_row_sum = BI.sum(axis = 0)
-            BI_col_sum = BI.sum(axis = 1)
-	    T = coo_matrix((-0.5*(BI_row_sum.getA1() + BI_col_sum.getA1()), (range(nsamples), range(nsamples))), shape=(nsamples, nsamples))
-            BI = BI + T
-            BI = BI + BI.T
-        else:
-            BI = coo_matrix(([], ([], [])), shape=(nsamples, nsamples))
-        
-        
-        if imposter_triplet.shape[0]>0:
-            CIs = Counter(imposter_triplet[...,0]*nsamples + imposter_triplet[...,2])
-            k = np.array(CIs.keys(), dtype='int')
-            v = np.array(CIs.values())*(-1)
-            i = k%nsamples
-            j = k/nsamples
-            CI = coo_matrix((v, (i,j)), shape=(nsamples, nsamples))
-            CI_row_sum = CI.sum(axis = 0)
-            CI_col_sum = CI.sum(axis = 1)
-            #CI.setdiag(-0.5*(CI_row_sum.getA1() + CI_col_sum.getA1()))
-	    T = coo_matrix((-0.5*(CI_row_sum.getA1() + CI_col_sum.getA1()), (range(nsamples), range(nsamples))), shape=(nsamples, nsamples))
-            CI = CI + T
-            CI = CI + CI.T
-        else:
-            CI = coo_matrix(([], ([], [])), shape=(nsamples, nsamples))
+                data0[i, j] = data[i, j] - 0.5 * epsilon
+                f0 = self.eval2(data0, triplet)
+                data0[i, j] = data[i, j] + 0.5 * epsilon
+                f1 = self.eval2(data0, triplet)
+                grad[i, j] = (f1 - f0) / epsilon
+        return grad
 
-        grad = (self.mu*AG + (1-self.mu)*(BI-CI))*data
-        
+    def forward_backward(self, data, label, k=3, center_update_lr=0., calculate_loss=True):
+        unique_label, label = np.unique(label, return_inverse = True)
+        label = label.astype('int32')
+        center_data = self.centers_[unique_label]
+
+        ext_data = np.concatenate((data, center_data), axis=0)
+        #print(label.shape, unique_label.shape)
+        ext_label = np.concatenate((label, np.arange(unique_label.shape[0],dtype='int32')), axis=0)
+        num_data = data.shape[0]
+        num_center_data = center_data.shape[0]
+        num_ext_data = ext_data.shape[0]
+
+        grad2 = ext_data.copy()
+        loss2 = vlc.verify_loss_c_py(X=ext_data, label = ext_label, num_extdata = unique_label.shape[0],
+                                          margin = self.margin_, k=k, grad = grad2)
+        print('loss2 = ', loss2)
+        print('grad2 = ', grad2)
+        # CPU版本的pairwise distance计算
+        # dist = pdg_cpu(ext_data, metric='sqeuclidean')
+        # GPU版本的pairwise distance计算
+        dist = np.zeros((num_ext_data, num_ext_data), dtype='float32')
+        pdg_gpu.pairwise_dist_gpu1(ext_data, dist)
+
+        inds = np.argpartition(dist[:num_data], k + 1)
+
+        dist_to_center = [dist[i, num_data + label[i]] for i in range(num_data)]
+        triplet = []
+        for i in range(num_data):
+            center_idx = num_data + label[i]
+            label_i = label[i]
+            t = []
+            d = []
+            l = []
+            for j in range(k + 1):
+                if dist[i, inds[i, j]] < dist_to_center[i] + self.margin_ and ext_label[inds[i, j]] != label_i:
+                    triplet.append([i, center_idx, inds[i, j]])
+                    t.append(inds[i,j])
+                    d.append(dist[i, inds[i, j]])
+            #print(i, 'label:', label_i, t, dist[i, inds[i,:k+1]], ext_label[inds[i,:k+1]])
+
+        A = np.zeros((num_ext_data, num_ext_data))
+        C = np.array([[0.75, -1.25, 0.5],
+                      [-1.25, 0.75, 0.5],
+                      [0.5, 0.5, -1.]])
+        for idx in triplet:
+            A[[[idx[0]], [idx[1]], [idx[2]]], idx] += C
+        """
+        print("A = ")
+        for i in range(num_ext_data):
+            print([x for x in A[i]])
+        """
+        grad = np.dot(A, ext_data)
+
+        """
+        print("grad = ")
+        for i in range(num_ext_data):
+            print([x for x in grad[i]])
+        """
+        if center_update_lr > 0:
+            self.centers_[unique_label] -= center_update_lr / num_data * grad[num_data:]
+        #print(self.centers_)
+
         if calculate_loss:
-            loss = self.eval(data, genuine_pair, genuine_pair_dist,  imposter_triplet, imposter_triplet_dist)
+            """
+            p = ext_data.reshape(-1)
+            q = grad.reshape(-1)
+            print('-------------------------------')
+            for i in range(p.shape[0]):
+                print("%f*%f"%(p[i], q[i]))
+            """
+            loss = 0.5 * sum(sum(ext_data * grad)) #self.eval(ext_data, A)
         else:
             loss = 0.
-        #grad2 = self.approx_gradient(data, genuine_pair, imposter_triplet)
-        #print('approx grad = ', grad2)
-        return loss, grad
-    def _select_knn(self, data, label, genuine_label, k):
-        self.k = k
-        self.k_inds = {}
-        self.k_dists = {}
-        for l in genuine_label:
-            inds, = np.nonzero(label == l)
-            #CPU版本的pairwise distance计算
-            dist = pdg(data[inds,...], metric='sqeuclidean')
-            #GPU版本的pairwise distance计算
-            #X = data[inds,...].astype('float32')
-	    #dist = np.zeros((X.shape[0], X.shape[0]), dtype='float32')
-	    #pdg.pairwise_dist_gpu1(X, dist)
-	    np.fill_diagonal(dist, np.inf)
-	    n1 = np.argpartition(dist, min(k, inds.shape[0]-1))
-	    dist = dist[[[i] for i in range(n1.shape[0])],n1[...,:min(k, inds.shape[0]-1)]]
-	    n2 = np.argsort(dist)
-	    nn = n1[[[i] for i in range(n1.shape[0])], n2]
-            self.k_inds[l] = inds[nn]
-            self.k_dists[l]= dist[[[i] for i in range(inds.shape[0])], n2]
-    def _select_genuine_pair(self, label, genuine_label):        
-        genuine_pair = []
-	genuine_pair_dist = 0
-        sample = Counter(np.random.randint(0, len(pos_label)))
-        for i in sample:
-            l = pos_label[i]
-            c = sample[i]
-            inds, = np.nonzero(label == l)
-            idx_a = inds[np.random.randint(0, inds.shape[0], c)]
-            idx_b = inds[np.random.randint(0, inds.shape[0], c)]
-            index = (idx_a != indx_b)
-            pos_pair = pos_pair + zip(idx_a[index], idx_b[index])
-            dist = data[idx_a[index]] - data[idx_b[index]]
-            pos_pair_dist +=  (dist*dist).sum()
-        """
-        for l in genuine_label:
-            inds, = np.nonzero(label == l)
-            k_inds = self.k_inds[l]
-            for i in range(len(inds)):
-                a = np.copy(k_inds[i,...])
-                a[:] = inds[i]
-                genuine_pair = genuine_pair + zip(a, k_inds[i,...]) 
-		genuine_pair_dist +=  self.k_dists[l][i,...].sum()
-        """           
-        return  np.array(genuine_pair), genuine_pair_dist
-                    
-    def _select_imposter_triplet(self, data, label, genuine_label):
-        imposter_triplet = []
-	imposter_triplet_dist = 0
-        for l in genuine_label:
-            index_a = self.k_inds[l][...,-1]
-            margin = self.k_dists[l][...,-1].flatten() + self.margin
-            in_inds, = np.nonzero(label == l)
-            out_inds, = np.nonzero(label != l)
-            #CPU版本的pairwise distance计算
-            dist = pdg(data[out_inds,...], data[in_inds,...],metric='sqeuclidean')
-            #GPU版本的pairwise distance计算
-            #X = data[out_inds,...].astype('float32')
-            #Y = data[in_inds,...].astype('float32')
-            #dist = np.zeros((X.shape[0], Y.shape[0]), dtype='float32')
-            #pdg.pairwise_dist_gpu2(X,Y, dist)
-            i,j = np.nonzero(dist >= margin)
-            dist[i,j] = np.inf
-            j = np.argpartition(dist, self.k, )[...,0:self.k].flatten()
-            i = np.tile(range(dist.shape[0]),[self.k,1]).T.flatten()
-            for p in zip(i,j):
-                if dist[p[0],p[1]] < np.inf:
-                        imposter_triplet.append([in_inds[p[1]], index_a[p[1]], out_inds[p[0]]])
-                        imposter_triplet_dist += (margin[p[1]]-dist[p[0],p[1]])        
-        return np.array(imposter_triplet), imposter_triplet_dist
-        
-    
-    def eval(self, data, genuine_pair, genuine_pair_dist, imposter_triplet, imposter_triplet_dist):
-	ret = 0
-        s1 = 0
-        s2 = 0
-	if genuine_pair_dist != 0:
-            s1 = genuine_pair_dist
-	else:
-            s1 = 0
-            for p in genuine_pair:
-		diff = data[p[0],:] - data[p[1],:]
-		s1 += (diff*diff).sum()   
-	ret +=  s1*0.5*self.mu
-	if imposter_triplet_dist != 0:
-            s2 = imposter_triplet_dist
-        else:
-            s2 = 0
-            for t in imposter_triplet:
-                diff1 = data[t[0],:] - data[t[1],:]
-                diff2 = data[t[0],:] - data[t[2],:]
-                s2 += (diff1*diff1).sum() + self.margin - (diff2*diff2).sum() 
-	ret += s2*0.5*(1-self.mu)                                           
+        # grad2 = self.approx_gradient(ext_data, triplet)
+        # print('approx grad = ', grad2, 'calculate grad', grad, ' diff ', np.linalg.norm(grad-grad2))
+        return loss, grad[:num_data], len(triplet)
+
+    def eval(self, data, A):
+        ret = 0.5 * sum(sum(data * (np.dot(A, data))))
         return ret
+
+    def eval2(self, data, triplet):
+        ret = 0.
+        for t in triplet:
+            x = data[t[0]]
+            y = data[t[1]]
+            z = data[t[2]]
+            ret += sum((x - y) * (x - y)) + self.margin_ - sum((z - 0.5 * (x + y)) * (z - 0.5 * (x + y)))
+        return 0.5 * ret
